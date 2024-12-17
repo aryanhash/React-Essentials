00b54ba29670945684c48dec34a0f99ba66597dd
Aryan Raj <avesh.arya12456@gmail.com>
2024-12-17 15:33:38 +0530

    Created a basic webapp in react

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton({ children, onSelect, isSelected}) {
+    
+  return <li>
+    <button onClick={onSelect} className={isSelected ? 'active' : ''}>{children}</button></li>;
+}
